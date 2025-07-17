9c17a18b3e751011a44566846aacec76f7aaf2e7
Vadym Tyemirov <temirov@users.noreply.github.com>
2025-07-17 16:01:15 -0700

    Add comprehensive Go documentation

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -6,7 +6,8 @@ import (
 
 var store *gsessions.CookieStore
 
-// NewSession creates a new cookie store
+// NewSession initializes the package-level cookie store with the given secret.
+// It should be called once at application startup.
 func NewSession(secret []byte) {
 	store = gsessions.NewCookieStore(secret)
 	store.Options = &gsessions.Options{
@@ -17,7 +18,8 @@ func NewSession(secret []byte) {
 	}
 }
 
-// Store returns the global session store
+// Store returns the global session store previously created with NewSession.
+// It panics if NewSession has not been called.
 func Store() *gsessions.CookieStore {
 	if store == nil {
 		panic("session store is nil")
